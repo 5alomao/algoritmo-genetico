5417a316fa3b5823e789f11bb134c1e2c8694870
5alomao <salomao.junior2k22@gmail.com>
2025-06-26 09:17:22 -0300

    feat: Ajustado projeto de Algoritmo Genetico.

--- src/AGMain.java
+++ src/AGMain.java
@@ -3,21 +3,23 @@ package agjava2025;
 public class AGMain {
 
     public static void main(String[] args) {
-        // AGBruteForce meuBruteForce = new AGBruteForce(8);
-        // meuBruteForce.carregaArquivo("dados.csv");
-        // meuBruteForce.resolver();
-        // meuBruteForce.mostrarMelhorSolucao();
-
-        int numeroGeracoes = 20;
+        int numeroGeracoes = 30;
         int tamanhoPopulacao = 100;
         int probabilidadeMutacao = 5;
-        int quantidadeCruzamento = 10;
-        double capacidadeMochila = 8.0;
+        int qtdeCruzamentos = 10;
+        double capacidadeMochila = 8;
+        AlgoritmoGenetico meuAg = new AlgoritmoGenetico(numeroGeracoes,
+                tamanhoPopulacao, probabilidadeMutacao,
+                qtdeCruzamentos, capacidadeMochila);
 
-        AlgoritmoGenetico algGen = new AlgoritmoGenetico(numeroGeracoes, tamanhoPopulacao, probabilidadeMutacao,
-                quantidadeCruzamento, capacidadeMochila);
-
-        algGen.carregaArquivo("dados.csv");
+        meuAg.carregaArquivo("dados50.csv");
+        meuAg.executar();
+        /*
+         * AGBruteForce meuBruteForce = new AGBruteForce(8);
+         * meuBruteForce.carregaArquivo("dados30.csv");
+         * meuBruteForce.resolver();
+         * meuBruteForce.mostrarMelhorSolucao();
+         */
     }
 
 }
--- src/AlgoritmoGenetico.java
+++ src/AlgoritmoGenetico.java
@@ -4,77 +4,65 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class AlgoritmoGenetico {
-    private int tamanhoCromossomo, numeroGeracoes, tamanhoPopulacao, probabilidadeMutacao, quantidadeCruzamento;
 
-    public int getTamanhoCromossomo() {
-        return tamanhoCromossomo;
-    }
-
-    public void setTamanhoCromossomo(int tamanhoCromossomo) {
-        this.tamanhoCromossomo = tamanhoCromossomo;
-    }
-
-    public int getNumeroGeracoes() {
-        return numeroGeracoes;
-    }
-
-    public void setNumeroGeracoes(int numeroGeracoes) {
-        this.numeroGeracoes = numeroGeracoes;
-    }
-
-    public int getTamanhoPopulacao() {
-        return tamanhoPopulacao;
-    }
-
-    public void setTamanhoPopulacao(int tamanhoPopulacao) {
-        this.tamanhoPopulacao = tamanhoPopulacao;
-    }
-
-    public int getProbabilidadeMutacao() {
-        return probabilidadeMutacao;
-    }
-
-    public void setProbabilidadeMutacao(int probabilidadeMutacao) {
-        this.probabilidadeMutacao = probabilidadeMutacao;
-    }
-
-    public int getQuantidadeCruzamento() {
-        return quantidadeCruzamento;
-    }
-
-    public void setQuantidadeCruzamento(int quantidadeCruzamento) {
-        this.quantidadeCruzamento = quantidadeCruzamento;
-    }
-
-    public double getCapacidadeMochila() {
-        return capacidadeMochila;
-    }
-
-    public void setCapacidadeMochila(double capacidadeMochila) {
-        this.capacidadeMochila = capacidadeMochila;
-    }
-
-    public ArrayList<Produto> getProdutos() {
-        return produtos;
-    }
-
-    public void setProdutos(ArrayList<Produto> produtos) {
-        this.produtos = produtos;
-    }
-
+    private int tamanhoCromossomo; // tamanho do vetor
+    private int numeroGeracoes;
+    private int tamanhoPopulacao;
+    private int probabilidadeMutacao;
+    private int qtdeCruzamentos;
     private double capacidadeMochila;
-    private ArrayList<Produto> produtos = new ArrayList<>();
 
-    public AlgoritmoGenetico(int numeroGeracoes, int tamanhoPopulacao,
-            int probabilidadeMutacao, int quantidadeCruzamento,
-            double capacidadeMochila) {
-        this.numeroGeracoes = numeroGeracoes;
-        this.tamanhoPopulacao = tamanhoPopulacao;
-        this.probabilidadeMutacao = probabilidadeMutacao;
-        this.quantidadeCruzamento = quantidadeCruzamento;
-        this.capacidadeMochila = capacidadeMochila;
+    private ArrayList<Produto> produtos = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> populacao = new ArrayList<>();
+
+    public AlgoritmoGenetico(int nGeracoes,
+            int tPopulacao, int probMutacao,
+            int qtCruzamentos, double capacidade) {
+
+        this.numeroGeracoes = nGeracoes;
+        this.tamanhoPopulacao = tPopulacao;
+        this.probabilidadeMutacao = probMutacao;
+        this.qtdeCruzamentos = qtCruzamentos;
+        this.capacidadeMochila = capacidade;
+    }
+
+    public void executar() {
+        criarPopulacao();
+        for (int i = 0; i < this.numeroGeracoes; i++) {
+            System.out.println("Geracao: " + i);
+            mostrarPopulacao();
+            operadoresGeneticos();
+            novaPopulacao();
+        }
+        ArrayList<Integer> melhor = obterMelhorSolucao();
+        mostrarMochila(melhor);
+    }
+
+    private ArrayList<Integer> obterMelhorSolucao() {
+        int indiceMelhor = obterMelhor();
+        ArrayList melhor = populacao.get(indiceMelhor);
+        System.out.println("Melhor solucao: " + melhor);
+        System.out.println("Avaliacao: " + fitness(melhor));
+        return melhor;
+    }
+
+    public void mostrarMochila(ArrayList<Integer> resultado) {
+        System.out.println("Avaliacao do Melhor:" + this.fitness(resultado));
+        // percorrer a solução e mostrar os produtos
+        System.out.println("Produtos levados na mochilha:");
+        for (int i = 0; i < resultado.size(); i++) {
+            int leva = resultado.get(i);
+            if (leva == 1) {
+                Produto p;
+                p = produtos.get(i);
+                System.out.println(p.getDescricao()
+                        + " Valor:" + p.getValor() + " Peso:" + p.getPeso());
+            } // fim if
+        } // fim for
+
     }
 
     public void carregaArquivo(String fileName) {
@@ -87,12 +75,157 @@ public class AlgoritmoGenetico {
                 novoProduto.setDescricao(produto[0]);
                 novoProduto.setPeso(Double.parseDouble(produto[1]));
                 novoProduto.setValor(Double.parseDouble(produto[2]));
+                System.out.println("Produto:" + novoProduto.getDescricao());
                 produtos.add(novoProduto);
             }
             this.tamanhoCromossomo = produtos.size();
             System.out.println("Total de produtos carregados: " + produtos.size());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private ArrayList<Integer> criarCromossomo() {
+        ArrayList<Integer> cromossomo = new ArrayList<>();
+        for (int i = 0; i < tamanhoCromossomo; i++) {
+            if (Math.random() < 0.6) {
+                cromossomo.add(0);
+            } else {
+                cromossomo.add(1);
+            }
+        }
+        return cromossomo;
+    }
+
+    private void criarPopulacao() {
+        for (int i = 0; i < this.tamanhoPopulacao; i++) {
+            this.populacao.add(criarCromossomo());
+        }
+    }
+
+    private double fitness(ArrayList<Integer> cromossomo) {
+        double peso = 0, beneficio = 0;
+        for (int i = 0; i < this.tamanhoCromossomo; i++) {
+            int leva = cromossomo.get(i);
+            if (leva == 1) {
+                Produto p = produtos.get(i);
+                peso += p.getPeso();
+                beneficio += p.getValor();
+            } // fim leva
+        } // fim for
+        if (peso <= this.capacidadeMochila) {
+            return beneficio;
+        } else {
+            return 0;
+        }
+    }
+
+    private int roleta() {
+        ArrayList roletaVirtual = new ArrayList();
+        for (int i = 0; i < this.tamanhoPopulacao; i++) {
+            double nota = fitness(populacao.get(i));
+            for (int j = 0; j <= nota; j++) {
+                roletaVirtual.add(i);
+            }
+        } // fim for
+        Random r = new Random();
+        int sorteado = r.nextInt(roletaVirtual.size());
+        return (int) roletaVirtual.get(sorteado);
+    }
+
+    private ArrayList cruzamento() {
+        ArrayList<Integer> filho1 = new ArrayList();
+        ArrayList<Integer> filho2 = new ArrayList();
+        ArrayList<ArrayList> filhos = new ArrayList();
+        ArrayList<Integer> p1, p2;
+        int ip1, ip2;
+        ip1 = roleta();
+        ip2 = roleta();
+        p1 = populacao.get(ip1);
+        p2 = populacao.get(ip2);
+        Random r = new Random();
+        int pos = r.nextInt(this.tamanhoCromossomo); // ponto de corte
+        for (int i = 0; i <= pos; i++) {
+            filho1.add(p1.get(i));
+            filho2.add(p2.get(i));
+        }
+        for (int i = pos + 1; i < this.tamanhoCromossomo; i++) {
+            filho1.add(p2.get(i));
+            filho2.add(p1.get(i));
+        }
+        filhos.add(filho1);
+        filhos.add(filho2);
+        return filhos;
+    }
+
+    private void mutacao(ArrayList filho) {
+        Random r = new Random();
+        int v = r.nextInt(100);
+        if (v < this.probabilidadeMutacao) {
+            int ponto = r.nextInt(this.tamanhoCromossomo);
+            if ((int) filho.get(ponto) == 1) {
+                filho.set(ponto, 0);
+            } else {
+                filho.set(ponto, 1);
+            }
+            System.out.println("Ocorreu mutação!");
+        } // fim if mutacao
+    }
+
+    private int obterPior() {
+        int indicePior = 0;
+        double pior, nota = 0;
+        pior = fitness((ArrayList) populacao.get(0));
+        for (int i = 1; i < this.tamanhoPopulacao; i++) {
+            nota = fitness((ArrayList) populacao.get(i));
+            if (nota < pior) {
+                pior = nota;
+                indicePior = i;
+            } // fim if
+        } // fim for
+        return indicePior;
+    }// fim funcao
+     // ---------------------------------
+
+    private int obterMelhor() {
+        int indiceMelhor = 0;
+        double melhor, nota = 0;
+        melhor = fitness(populacao.get(0));
+        for (int i = 1; i < this.tamanhoCromossomo; i++) {
+            nota = fitness(populacao.get(i));
+            if (nota > melhor) {
+                melhor = nota;
+                indiceMelhor = i;
+            } // fim if
+        } // fim for
+        return indiceMelhor;
+    }// fim obter melhor
+
+    private void operadoresGeneticos() {
+        ArrayList f1, f2;
+        ArrayList<ArrayList> filhos;
+        for (int i = 0; i < this.qtdeCruzamentos; i++) {
+            filhos = cruzamento();
+            f1 = filhos.get(0);
+            f2 = filhos.get(1);
+            mutacao(f1);
+            mutacao(f2);
+            populacao.add(f1);
+            populacao.add(f2);
+        }
+    }
+
+    private void novaPopulacao() {
+        for (int i = 0; i < this.qtdeCruzamentos; i++) {
+            this.populacao.remove(obterPior());
+            this.populacao.remove(obterPior());
+        }
+    }
+
+    public void mostrarPopulacao() {
+        for (int i = 0; i < this.tamanhoPopulacao; i++) {
+            System.out.println("Cromossomo " + i + ": " + populacao.get(i));
+            System.out.println("Avaliação:" + fitness(populacao.get(i)));
         }
     }
 
